e1ae032efd84d2718e6c12309fa1ba02a7e8d192
24HourDeveloper <latoya30310@gmail.com>
2022-11-07 20:44:15 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
+
+
+app.listen(3001,() => {
+  console.log("My Server is running 3001");
+})
